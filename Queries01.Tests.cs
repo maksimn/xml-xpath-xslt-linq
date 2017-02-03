9250993ba7e582e4b4b95a952a6b88f4b78ab91e
maksimn <maksim1989sovunion@yandex.ru>
2017-02-03 15:51:57 +0300

    Первый LINQ-to-XML запрос

---
+++ Queries01.Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+using NUnit.Framework;
+
+using ShriDzXslt;
+
+[TestFixture]
+class Queries01xml {
+    [Test]
+    public void Query01_PersonsWithPhone() {
+        var xmlFilePath = AppDomain.CurrentDomain.BaseDirectory + "..\\..\\01.xml";
+
+        var persons = Queries01.PersonsWithPhone(xmlFilePath);
+        
+        var num = persons.Count();
+        
+        Assert.AreEqual(3, num);
+    }
+}
---
+++ Queries01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ShriDzXslt {
+    public class Queries01 {
+        public static IEnumerable<XElement> PersonsWithPhone(string xmlFile) {
+            XElement root = XElement.Load(xmlFile);
+
+            IEnumerable<XElement> persons =  
+                from person in root.Elements("person")
+                where person.Descendants(XName.Get("phone")).Count() >= 1
+                select person;
+ 
+            return persons;
+        }
+    }
+}
